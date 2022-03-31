6a016d0b776fe6881d7ad1d86cec88290ec9bcc1
vanta blanta <vantablanta@gmail.com>
2022-03-31 21:06:47 +0300

    type declaration

--- hello.js
+++ hello.js
@@ -1,1 +1,3 @@
 console.log('I am a Typescript example');
+var firstName = "Michelle";
+console.log(firstName);
--- hello.ts
+++ hello.ts
@@ -1,1 +1,3 @@
 console.log('I am a Typescript example');
+var firstName: string = "Michelle";
+console.log(firstName);
