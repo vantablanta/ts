062bcdea4d69676fe8ec272c9d4ae1d9b112e7dd
vanta blanta <vantablanta@gmail.com>
2022-04-01 10:26:45 +0300

    Tuples with type oneline

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
 console.log('I am a Typescript example');
 var firstName = "Michelle";
 console.log(firstName);
+var myTuple = ["Michelle", 25, true];
+console.log(myTuple);
--- hello.ts
+++ hello.ts
@@ -1,3 +1,6 @@
 console.log('I am a Typescript example');
 var firstName: string = "Michelle";
 console.log(firstName);
+
+var myTuple: [string, number, boolean] = ["Michelle", 25, true]
+console.log(myTuple)
