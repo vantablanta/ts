9bd5f560baafd3455d6f1f771a0ed7f284d20a79
vanta blanta <vantablanta@gmail.com>
2022-04-01 11:01:18 +0300

    interfaces and types

--- hello.ts
+++ hello.ts
@@ -12,9 +12,52 @@ var car: {type:string, model: string, year: number} = {
 }
 
 enum compassPoints {
-    North,
-    East,
-    West,
-    South
+    North = "North",
+    East = "East",
+    West = "West",
+    South = "South"
 }
 console.log(compassPoints.North)
+
+
+type Year = number
+type Type = string
+type Model = string
+
+type Car ={
+    year:  Year,
+    type:  Type,
+    model: Model
+}
+const carYear: Year = 2001
+const carType: Type = "Toyota"
+const carModel: Model = "Corolla"
+
+const carObj: Car = {
+  year: carYear,
+  type: carType,
+  model: carModel
+};
+
+
+// Interfaces appply to objects only 
+
+interface Rectangle {
+    height: number,
+    width: number
+}
+
+const rectangle: Rectangle = {
+    height: 20,
+    width: 10
+}
+
+interface BlueRectangle extends Rectangle {
+    color: string
+}
+
+const blueRectangle: BlueRectangle = {
+    height: 20,
+    width: 10,
+    color: "blue"
+}
